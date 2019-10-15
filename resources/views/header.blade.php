d7d0999a833366328a32c71a50414e44b101d2d2
hoangbui0808 <44484909+hoangbui0808@users.noreply.github.com>
2019-10-15 10:52:54 +0700

    a

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -5,14 +5,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
     <title>BUZZSHOP-SỰ KHÁC BIỆT CỦA BẠN</title>
-    <base href="{{asset('')}}">
-
     <meta name="keywords" content="thời trang Buzzshop" />
     <meta name="description" content="khác biệt với Buzzshop">
     <meta name="author" content="">
-        
+    <base href="{{asset('')}}">  
     <!-- Favicon -->
     <link rel="icon" type="image/x-icon" href="assets/images/icons/favicon.ico">
     
